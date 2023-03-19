a99ecb7c9fcd09dd9e3d64f111cdcbdccc459185
Fabian Castano <fabioc9675@gmail.com>
2023-03-18 20:30:36 -0500

    Runing react app

--- greenhouse-b4a/index.js
+++ greenhouse-b4a/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.use("/", (req, res) => {
-    res.send("Hello World from back4app Containers!");
+    res.send("Hello World from back4app Containers, I'm Fabian!");
 });
 
 app.listen(3333, () => {
